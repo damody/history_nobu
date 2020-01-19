6bc24652ecda42ebe25198026e122200b773ba8b
ts0963187279 <ts0963187279@gmail.com>
2020-01-19 13:46:40 +0800

    increase hp of cavalry and gunner

--- game/dota_addons/nobu/scripts/vscripts/computer_system/chubing.lua
+++ game/dota_addons/nobu/scripts/vscripts/computer_system/chubing.lua
@@ -335,7 +335,7 @@ function ShuaGuai_Of_B(num, team, pos)
 				for x=1,n do
 					local unit = CreateUnitByName(unit_name, ShuaGuai_entity_point[i] , true, nil, nil, team)
 					unit:AddAbility("set_level_1"):SetLevel(1)
-					local hp = (unit:GetMaxHealth()+300) * intensify
+					local hp = (unit:GetMaxHealth()+600) * intensify
 					unit:SetBaseMaxHealth(hp+A_count * 5)
 					local dmgmax = (unit:GetBaseDamageMax()) * intensify
 					local dmgmin = (unit:GetBaseDamageMin()) * intensify
@@ -413,7 +413,7 @@ function ShuaGuai_Of_C(num, team, pos)
 				for x=1,n do
 					local unit = CreateUnitByName(unit_name, ShuaGuai_entity_point[i] , true, nil, nil, team)
 					
-					local hp = (unit:GetMaxHealth()+800) * intensify
+					local hp = (unit:GetMaxHealth()) * intensify
 					unit:SetBaseMaxHealth(hp+A_count * 10)
 					local dmgmax = (unit:GetBaseDamageMax()) * intensify
 					local dmgmin = (unit:GetBaseDamageMin()) * intensify
