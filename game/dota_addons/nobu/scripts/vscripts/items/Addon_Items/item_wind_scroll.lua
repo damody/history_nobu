6c032cd6d8e5c677fa83dbcb9ffbd730a10a26ea
ts0963187279 <ts0963187279@gmail.com>
2020-02-13 13:39:06 +0800

    item_art_of_war series share CD vertically

--- game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_wind_scroll.lua
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_wind_scroll.lua
@@ -66,5 +66,13 @@ function Shock( keys )
 			return 1	
 		end
 	end)
-
+	for itemSlot = 0,8 do
+		local item = caster:GetItemInSlot(itemSlot)
+		if item ~= nil and ((item:GetName() == "item_wind_scroll") or
+				(item:GetName() == "item_the_art_of_war_tornado_chapter") or 
+				(item:GetName() == "item_the_lost_art_of_war_2") or 
+				(item:GetName() == "item_recipe_the_art_of_war"))then
+			item:StartCooldown(ability:GetCooldown(-1))
+		end
+	end
 end
