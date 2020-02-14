e988f09e581eb08faf76cab1a917c7939df18ec8
ts0963187279 <ts0963187279@gmail.com>
2020-02-14 19:57:25 +0800

    add new item : earth_wall_book : +10 int create a lot rocks to hint enemy

--- game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_earth_wall_book.lua
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_earth_wall_book.lua
@@ -2,11 +2,10 @@
 -- item_earth_wall_book
 function Shock( keys )
 	local caster = keys.caster
-	local center = keys.target_points[1]
+	local center = caster:GetCursorPosition()
 	local ability = keys.ability
 	local aoe_radius = ability:GetLevelSpecialValueFor("aoe_radius",0)
 	local duration = ability:GetLevelSpecialValueFor("duration",0)
-
 	-- 搜尋參數
 	local iTeam = caster:GetTeamNumber()
 	local target_team = DOTA_UNIT_TARGET_TEAM_BOTH
@@ -58,22 +57,40 @@ function Shock( keys )
 
 	end
 
-	local wall_angle = VectorToAngles(center-caster:GetAbsOrigin()).y + 90
+	local wall_angle = VectorToAngles(center-caster:GetAbsOrigin()).y
+	-- local wall_angle = VectorToAngles(caster:GetForwardVector())
 	local dx = math.cos(wall_angle*(3.14/180))
 	local dy = math.sin(wall_angle*(3.14/180))
 	local wall_offset = Vector(dx,dy,0) * (aoe_radius)
 	
 	
 	-- 產生兩個石牆單位
-	local wall1 = CreateUnitByName("EARTH_WALL_hero",center+wall_offset,false,nil,nil,caster:GetTeam())
-	wall1:AddNewModifier(caster,nil,"modifier_kill",{duration=duration})
-	local wall2 = CreateUnitByName("EARTH_WALL_hero",center-wall_offset,false,nil,nil,caster:GetTeam())
-	wall2:AddNewModifier(caster,nil,"modifier_kill",{duration=duration})
-	local wall3 = CreateUnitByName("EARTH_WALL_hero",center,false,nil,nil,caster:GetTeam())
-	wall3:AddNewModifier(caster,nil,"modifier_kill",{duration=duration})
-	wall1:RemoveModifierByName("modifier_invulnerable")
-	wall2:RemoveModifierByName("modifier_invulnerable")
-	wall3:RemoveModifierByName("modifier_invulnerable")
+	local rock_counter = 0
+	local origin = caster:GetAbsOrigin()
+	Timers:CreateTimer(0,function()
+		rock_counter = rock_counter + 1
+		if rock_counter > 8 then return nil end
+			local rock_center = origin+wall_offset * (1 + rock_counter)
+			local wall = CreateUnitByName("EARTH_WALL_hero",rock_center,false,nil,nil,caster:GetTeam())
+			wall:AddNewModifier(caster,nil,"modifier_kill",{duration=duration})
+			wall:RemoveModifierByName("modifier_invulnerable")
+			local units = FindUnitsInRadius(iTeam,	-- 關係
+							 rock_center,-- 搜尋的中心點
+                             nil, 				-- 好像是優化用的參數不懂怎麼用
+                             200,		-- 搜尋半徑
+                             target_team,		-- 目標隊伍
+                             target_type,		-- 目標類型
+                             target_flags,		-- 額外選擇或排除特定目標
+                             FIND_ANY_ORDER,	-- 結果的排列方式
+                             false) 			-- 好像是優化用的參數不懂怎麼用
+
+			-- 處理搜尋結果
+			for _,unit in ipairs(units) do
+				-- 避免卡住
+				unit:AddNewModifier(nil,nil,"modifier_phased",{duration=0.01})
+			end		
+		return 0.15
+	end)
 end
 
 function Shock2( keys )
