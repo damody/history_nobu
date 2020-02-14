873589c666ca79b6bffe5da83b85da7d2836eab0
ts0963187279 <ts0963187279@gmail.com>
2020-02-14 22:45:40 +0800

    update item_ronin_jitte : (active) block melee attack 10 times or 5second

--- game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_ronin_jitte.lua
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_ronin_jitte.lua
@@ -4,11 +4,27 @@ function Shock( keys )
 	local caster = keys.caster
 	local target = keys.target
 	local ability = keys.ability
-	local dmg = keys.dmg
-	
-	AMHC:Damage( caster,target,dmg + caster:GetMaxMana()*0.12,AMHC:DamageType( "DAMAGE_TYPE_MAGICAL" ) )
-	ParticleManager:CreateParticle("particles/generic_gameplay/illusion_killed_halo.vpcf",PATTACH_ABSORIGIN_FOLLOW, target)
-	ParticleManager:CreateParticle("particles/generic_gameplay/illusion_killed_halo.vpcf",PATTACH_ABSORIGIN_FOLLOW, target)
-	ability:ApplyDataDrivenModifier(caster, target, "modifier_lightning_katana", {duration = 2})
+	local max_counter = ability:GetSpecialValueFor("max_counter")
+	local duration = ability:GetSpecialValueFor("duration")
+	ability:ApplyDataDrivenModifier(caster, caster,"modifier_melee_block",{duration=duration})
+	local modifier = caster:FindModifierByName("modifier_melee_block")
+	modifier:SetStackCount(max_counter)
 end
 
+function OntakeDamage( keys )
+	local target = keys.attacker
+	local caster = keys.caster
+	local ability = keys.ability
+	local damage = keys.DamageTaken
+	--DeepPrintTable(keys)
+	local distance = (caster:GetAbsOrigin() - target:GetAbsOrigin()):Length()
+	print(distance)
+	local modifier = caster:FindModifierByName("modifier_melee_block")
+	if distance < 200 and modifier:GetStackCount() > 0 then
+		modifier:SetStackCount(modifier:GetStackCount() - 1)
+		caster:SetHealth(caster:GetHealth() + damage)
+	end
+	if modifier:GetStackCount() == 0 then
+		caster:RemoveModifierByName("modifier_melee_block")
+	end
+end
