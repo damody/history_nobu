7f881e058e839a116b7863533198e5a6c66615e5
Victor <ts0963187279@gmail.com>
2020-02-20 13:49:25 +0800

    update item great sword of soul hyper : atk 30 => 50, dex 30 => 20, str 10 => 0, int 10 => 0, stunned enemy 2.5 second and increase physical damage taken even magic immune

--- game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_the_great_sword_of_soul.lua
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_the_great_sword_of_soul.lua
@@ -9,5 +9,9 @@ function Shock2( keys )
 	local caster = keys.caster
 	local target = keys.target
 	local ability = keys.ability
-	ability:ApplyDataDrivenModifier(caster,target,"modifier_the_great_sword_of_soul_hyper",nil)
+	if target:IsMagicImmune() then
+		ability:ApplyDataDrivenModifier(caster,target,"modifier_the_great_sword_of_soul_hyper",nil)
+	else
+		ability:ApplyDataDrivenModifier(caster,target,"modifier_the_great_sword_of_soul_hyper",nil)
+	end
 end
