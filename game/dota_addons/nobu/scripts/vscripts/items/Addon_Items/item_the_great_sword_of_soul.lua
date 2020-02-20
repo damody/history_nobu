deb50151a2dc43ea242b23451cb71e55065f6797
Victor <ts0963187279@gmail.com>
2020-02-20 22:36:51 +0800

    update item great sword of soul : increase physical damage taken duration 2.5 => 8

--- game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_the_great_sword_of_soul.lua
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_the_great_sword_of_soul.lua
@@ -11,7 +11,9 @@ function Shock2( keys )
 	local ability = keys.ability
 	if target:IsMagicImmune() then
 		ability:ApplyDataDrivenModifier(caster,target,"modifier_the_great_sword_of_soul_hyper",nil)
+		ability:ApplyDataDrivenModifier(caster,target,"modifier_stunned",{duration = 2.5})
 	else
 		ability:ApplyDataDrivenModifier(caster,target,"modifier_the_great_sword_of_soul_hyper",nil)
+		ability:ApplyDataDrivenModifier(caster,target,"modifier_stunned",{duration = 2.5})
 	end
 end
