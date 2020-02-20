6ebe08c38469d6c11ce566e86eb0065f6dfe9577
Victor <ts0963187279@gmail.com>
2020-02-20 14:50:04 +0800

    update item muramasa_katana : atk 120 => kill_count , +5% life leech and 10% attack speed when attack can stack 10 times

--- game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_muramasa_katana.lua
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_muramasa_katana.lua
@@ -1,9 +1,12 @@
 
 function OnEquip( keys )	
 	local caster = keys.caster
+	local ability = keys.ability
+	ability.attackCount = 0
 	if (caster.nobuorb1 == nil) then
 		caster.nobuorb1 = "muramasa_katana"
 	end
+	caster:FindModifierByName("modifier_muramasa_atk"):SetStackCount(caster.kill_count)
 end
 
 function OnUnequip( keys )	
@@ -11,13 +14,27 @@ function OnUnequip( keys )
 	if (caster.nobuorb1 == "muramasa_katana") then
 		caster.nobuorb1 = nil
 	end
+	caster:RemoveModifierByName("modifier_muramasa_atk")
 end
 
+function KillCount( keys )
+	local caster = keys.caster
+	if caster:IsAlive() then
+		caster:FindModifierByName("modifier_muramasa_atk"):SetStackCount(caster.kill_count)
+	end
+end
 
 function StealLife(keys)
 	--【Basic】
 	local caster = keys.caster
 	local target = keys.target
+	local ability = keys.ability
+	if not caster:FindModifierByName("modifier_muramasa_atk_speed") then ability.attackCount = 0 end
+	ability:ApplyDataDrivenModifier(caster , caster, "modifier_muramasa_atk_speed", nil)
+	ability.attackCount = ability.attackCount + 1
+	if ability.attackCount > 10 then  ability.attackCount = 10 end
+	local modifier = caster:FindModifierByName("modifier_muramasa_atk_speed")
+	modifier:SetStackCount(ability.attackCount)
 	if target.isvoid == nil then
 		local ability = keys.ability
 		local level = ability:GetLevel() - 1
@@ -40,7 +57,7 @@ function StealLife(keys)
 			if damageReduction > 1 then
 				damageReduction = 1
 			end
-			caster:Heal(dmg*keys.StealPercent*0.01*(1-damageReduction), ability)
+			caster:Heal(dmg*ability.attackCount*0.05*(1-damageReduction), ability)
 		    ParticleManager:CreateParticle("particles/generic_gameplay/generic_lifesteal.vpcf",PATTACH_ABSORIGIN_FOLLOW, caster)
 		end
 	end
