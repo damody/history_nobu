93d516d5ae5a6f0c45ea0b99e02737415e6c0585
Victor <ts0963187279@gmail.com>
2020-02-25 17:33:19 +0800

    update item_the_great_sword_of_toxic : slow enemy 60% => 35%(melee) 25%(range)

--- game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_the_great_sword_of_toxic.lua
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_the_great_sword_of_toxic.lua
@@ -49,9 +49,13 @@ function Shock2( keys )
 	local caster = keys.caster
 	local target = keys.target
 	local ability = keys.ability
-
+	print(caster:GetBaseAttackRange())
 	if not target:IsBuilding() then
-		ability:ApplyDataDrivenModifier(target,target,"modifier_the_great_sword_of_toxic",nil)
+		if caster:GetBaseAttackRange() < 200 then
+			ability:ApplyDataDrivenModifier(target,target,"modifier_the_great_sword_of_toxic_melee",nil)
+		else
+			ability:ApplyDataDrivenModifier(target,target,"modifier_the_great_sword_of_toxic_range",nil)
+		end
 		ability:ApplyDataDrivenModifier(caster,target,"modifier_the_great_sword_of_toxic2",nil)
 		AMHC:Damage( caster,target,85,AMHC:DamageType( "DAMAGE_TYPE_MAGICAL" ) )
 	end
