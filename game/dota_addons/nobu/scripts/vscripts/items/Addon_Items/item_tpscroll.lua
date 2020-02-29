eb80f02fd1a075b12bc9809e4432ec1e5f670769
Victor <ts0963187279@gmail.com>
2020-02-29 15:24:18 +0800

    update item tpscroll can interrupt by cast again

--- game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_tpscroll.lua
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_tpscroll.lua
@@ -6,7 +6,12 @@ function Shock( keys )
 		caster:AddAbility("war_magic_gohome"):SetLevel(1)
 	end
 	local skill = caster:FindAbilityByName("war_magic_gohome")
-	skill:ApplyDataDrivenModifier(caster, caster, "modifier_wantgohome", {duration = 10})
+	if caster:HasModifier("modifier_wantgohome") then
+		caster:RemoveModifierByName("modifier_wantgohome")
+		caster:RemoveModifierByName("modifier_gohomelua")
+		return
+	end
+	skill:ApplyDataDrivenModifier(caster, caster, "modifier_wantgohome", {duration = 8.5})
 	if (caster:GetTeamNumber() == DOTA_TEAM_GOODGUYS) then
 		GameRules: SendCustomMessage("<font color=\"#cc3333\">織田軍即將發動召集戰法</font>", DOTA_TEAM_GOODGUYS + DOTA_TEAM_BADGUYS, 0)
 	else
@@ -14,19 +19,29 @@ function Shock( keys )
 	end
 	target:AddNewModifier(target,ability,"modifier_gohomelua",{duration=8})
 	target:FindModifierByName("modifier_gohomelua").caster = target
-	Timers:CreateTimer(8, function()
-		if target:HasModifier("modifier_wantgohome") then
-			if (caster:GetTeamNumber() == DOTA_TEAM_GOODGUYS) then
-				GameRules: SendCustomMessage("<font color=\"#cc3333\">織田軍發動召集戰法</font>", DOTA_TEAM_GOODGUYS + DOTA_TEAM_BADGUYS, 0)
-			else
-				GameRules: SendCustomMessage("<font color=\"#cc3333\">聯合軍發動召集戰法</font>", DOTA_TEAM_BADGUYS + DOTA_TEAM_GOODGUYS, 0)
+	local count = 0
+	Timers:CreateTimer(0, function()
+		if not target:HasModifier("modifier_wantgohome") then
+			return nil
+		end
+		if count >= 8 then
+			--ParticleManager:DestroyParticle(particle,false)
+			if target:HasModifier("modifier_wantgohome") then
+				if (caster:GetTeamNumber() == DOTA_TEAM_GOODGUYS) then
+					GameRules: SendCustomMessage("<font color=\"#cc3333\">織田軍發動召集戰法</font>", DOTA_TEAM_GOODGUYS + DOTA_TEAM_BADGUYS, 0)
+				else
+					GameRules: SendCustomMessage("<font color=\"#cc3333\">聯合軍發動召集戰法</font>", DOTA_TEAM_BADGUYS + DOTA_TEAM_GOODGUYS, 0)
+				end
+				target:SetTimeUntilRespawn(0)
+				target:AddNewModifier(target,ability,"modifier_phased",{duration=0.1})
+				Timers:CreateTimer(1, function()
+					target:RemoveModifierByName("modifier_wantgohome")
+					target:RemoveModifierByName("modifier_gohomelua")
+				end)
 			end
-			target:SetTimeUntilRespawn(0)
-			target:AddNewModifier(target,ability,"modifier_phased",{duration=0.1})
-			Timers:CreateTimer(1, function()
-				target:RemoveModifierByName("modifier_wantgohome")
-				target:RemoveModifierByName("modifier_gohomelua")
-			end)
+		else
+			count = count + 0.5
+			return 0.5
 		end
 	end)
 end
