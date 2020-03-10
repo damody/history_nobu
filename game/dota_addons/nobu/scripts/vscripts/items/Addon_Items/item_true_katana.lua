c2641c957e605b5147c5bfd4c2f3c3c6803f7552
Victor <ts0963187279@gmail.com>
2020-03-10 20:48:39 +0800

    add file to item true katana

---
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_true_katana.lua
@@ -0,0 +1,9 @@
+function chance_cannot_miss( keys )
+    local ability = keys.ability
+    local caster = keys.caster
+    local random = RandomInt(1,100)
+    if random <= 65 then
+        local rate = 1/caster:GetAttacksPerSecond()
+        ability:ApplyDataDrivenModifier(caster,caster,"modifier_chance_cannot_miss",{duration = rate})
+    end
+end
