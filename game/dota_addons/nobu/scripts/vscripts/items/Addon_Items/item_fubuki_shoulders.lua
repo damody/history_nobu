6c2b7d5b02d5de369107b3a679f59b30674747f6
Victor <ts0963187279@gmail.com>
2020-03-17 16:52:47 +0800

    update item_fubuki_shoulders can not slow building and magic immune target

--- game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_fubuki_shoulders.lua
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_fubuki_shoulders.lua
@@ -39,10 +39,12 @@ function modifier_fubuki_shoulders:OnTakeDamage(event)
 		        local dmg = event.damage
 				local healmax = dmg*0.30
 				local mana = healmax / 2.5
-
-				ability:ApplyDataDrivenModifier(self.caster, victim, "modifier_slow_move_speed", {})
-				ability:ApplyDataDrivenModifier(self.caster, victim, "modifier_slow_attack_speed", {})
-				victim:FindModifierByName("modifier_slow_attack_speed"):SetStackCount(self.caster:GetLevel())
+				if victim:IsBuilding() or victim:IsMagicImmune() then
+				else
+					ability:ApplyDataDrivenModifier(self.caster, victim, "modifier_slow_move_speed", {})
+					ability:ApplyDataDrivenModifier(self.caster, victim, "modifier_slow_attack_speed", {})
+					victim:FindModifierByName("modifier_slow_attack_speed"):SetStackCount(self.caster:GetLevel())
+				end
 				if (self.caster:GetMana() >= mana and self.caster:GetHealth() > healmax) then
 					self.caster:SpendMana(mana,ability)
 					self.caster:SetHealth(self.caster:GetHealth() + healmax)
@@ -66,7 +68,7 @@ function Start( keys )
 	ParticleManager:SetParticleControl(particle, 4, Vector(shield_size,0,shield_size))
 	ParticleManager:SetParticleControl(particle, 5, Vector(shield_size,0,0))
 	ParticleManager:SetParticleControlEnt(particle, 0, caster, PATTACH_POINT_FOLLOW, "attach_hitloc", caster:GetAbsOrigin(), true)
-	Timers:CreateTimer(7, function ()
+	Timers:CreateTimer(15, function ()
 		ParticleManager:DestroyParticle(particle, true)
 		end)
 end
