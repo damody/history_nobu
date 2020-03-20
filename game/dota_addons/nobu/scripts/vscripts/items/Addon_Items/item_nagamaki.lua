9dc60bc1cf42a8bac4169d5aba9e01bffdfb5c14
Victor <ts0963187279@gmail.com>
2020-03-20 19:06:24 +0800

    update item nagamaki : sarcifi 10% current life => 10% maximum life

--- game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_nagamaki.lua
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_nagamaki.lua
@@ -1,6 +1,12 @@
 function Shock ( keys )
     local caster = keys.caster
     local ability = keys.ability
+    local max_hp = caster:GetMaxHealth()
     local hp = caster:GetHealth()
-    caster:SetHealth(hp * 0.9)
+    if hp - max_hp * 0.1 < 0 then
+        ability:EndCooldown()
+        return 
+    end
+    caster:SetHealth(hp - max_hp * 0.1)
+    ability:ApplyDataDrivenModifier(caster, caster, "modifier_nagamaki", {duration = 10})
 end
