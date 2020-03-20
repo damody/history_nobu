25f84663e6ce2eb02176680cb176d6a46f2ea682
Victor <ts0963187279@gmail.com>
2020-03-20 19:32:56 +0800

    update item the great sword of toxic : add new feature when active : mute enemy 3 second

--- game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_the_great_sword_of_toxic.lua
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_the_great_sword_of_toxic.lua
@@ -7,6 +7,7 @@ function Shock( keys )
 	AMHC:Damage( caster,target,dmg,AMHC:DamageType( "DAMAGE_TYPE_MAGICAL" ) )
 	local mod = "particles/b33/b33r_old_poison.vpcf"
 	local part = ParticleManager:CreateParticle(mod, PATTACH_ABSORIGIN, target)
+	ability:ApplyDataDrivenModifier(caster, target, "modifier_the_great_sword_of_toxic3", {})
 	Timers:CreateTimer( 0,function ()
 		if int <= 4 then
 			int = int + 1
