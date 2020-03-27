8a6a10645e666e81c4cbe6af96edd3f3a92db99a
Victor <ts0963187279@gmail.com>
2020-03-27 15:41:07 +0800

    update item spear of saddle : level -7 armor , level 2 -12 armor , level 3 -15 armor

--- game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_the_spear_of_saddle.lua
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_the_spear_of_saddle.lua
@@ -25,24 +25,11 @@ end
 
 function Shock( keys )
 	local caster = keys.caster
-	local skill = keys.ability
+	local ability = keys.ability
 	local target = keys.target
-	local armor_reduce_percentage = skill:GetSpecialValueFor("ARMOR_REDUCE_PERCENTAGE")
-	local armor = target:GetPhysicalArmorValue(false)
-	print(armor)
-	if target:HasModifier("modifier_spear_of_saddle") then
-		armor = armor/(1 - armor_reduce_percentage * 0.01)
-	end
+	local armor_reduction = ability:GetSpecialValueFor("armor_reduction")
 	if not target:IsBuilding() then
-		skill:ApplyDataDrivenModifier(caster, keys.target,"modifier_spear_of_saddle",{ duration = 1 })
-		local modifier = target:FindModifierByName("modifier_spear_of_saddle")
-		if modifier ~= nil then
-			if armor > 0 then 
-				modifier:SetStackCount(armor*armor_reduce_percentage*0.01)
-			else
-				target:RemoveModifierByName("modifier_spear_of_saddle")
-			end
-		end
+		ability:ApplyDataDrivenModifier(caster, keys.target,"modifier_spear_of_saddle",{ duration = 1.5 })
 	end
 end
 
