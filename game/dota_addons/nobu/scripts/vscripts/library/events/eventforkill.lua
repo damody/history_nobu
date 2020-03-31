67df3f052dad1b1ed4d9a9bee12a2ef6b2621a72
Victor <ts0963187279@gmail.com>
2020-04-01 01:16:04 +0800

    update : get more exp when kill hero

--- game/dota_addons/nobu/scripts/vscripts/library/events/eventforkill.lua
+++ game/dota_addons/nobu/scripts/vscripts/library/events/eventforkill.lua
@@ -125,7 +125,8 @@ function Nobu:OnUnitKill( keys )
       end
       if killedUnit:IsHero() and not killedUnit:IsIllusion() then
         AttackerUnit.kill_hero_count = AttackerUnit.kill_hero_count + 1
-
+        --拿金驗
+        AttackerUnit:AddExperience(killedUnit:GetLevel()*10, 0, false, false)
         --連殺獎勵
         local sk_kill = 1
         if AttackerUnit.sk_kill then
@@ -147,10 +148,8 @@ function Nobu:OnUnitKill( keys )
           GameRules:SendCustomMessage("<font color='#ffff00'>".._G.hero_name_zh[nobu_id].."中止了".._G.hero_name_zh[nobu_id2].."的連殺，得到"..(killedUnit.sk_kill*50).."獎勵</font>",0,0)
         end
         --屯錢獎勵
-        print(killedUnit:GetGold())
         if killedUnit:GetGold() > 3000 then
           local level = math.floor((killedUnit:GetGold()-3000)/1000)
-          print(level)
           AMHC:GivePlayerGold_UnReliable(AttackerUnit:GetPlayerOwnerID(), (2 + level)*50)
           local nobu_id = _G.heromap[AttackerUnit:GetName()]
           local nobu_id2 = _G.heromap[killedUnit:GetName()]
