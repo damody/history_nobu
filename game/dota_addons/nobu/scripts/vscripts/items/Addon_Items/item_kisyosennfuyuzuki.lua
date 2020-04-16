0b49ae097c685701138b46e4ed4616d830513380
Victor <ts0963187279@gmail.com>
2020-04-16 15:27:41 +0800

    item_kisyosennfuyuzuki not effect to illusion

--- game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_kisyosennfuyuzuki.lua
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_kisyosennfuyuzuki.lua
@@ -1,22 +1,26 @@
 local shield_size = 75
 function OnEquip( keys )
-    local caster = keys.caster
-    local ability = keys.ability
-    caster.kisyosennfuyuzuki_shield = ParticleManager:CreateParticle("particles/item_kisyosennfuyuzuki/item_kisyosennfuyuzuki.vpcf", PATTACH_ABSORIGIN_FOLLOW, caster)
-	ParticleManager:SetParticleControl(caster.kisyosennfuyuzuki_shield, 1, Vector(shield_size,0,shield_size))
-	ParticleManager:SetParticleControl(caster.kisyosennfuyuzuki_shield, 2, Vector(shield_size,0,shield_size))
-	ParticleManager:SetParticleControl(caster.kisyosennfuyuzuki_shield, 4, Vector(shield_size,0,shield_size))
-	ParticleManager:SetParticleControl(caster.kisyosennfuyuzuki_shield, 5, Vector(shield_size,0,0))
-    ParticleManager:SetParticleControlEnt(caster.kisyosennfuyuzuki_shield, 0, caster, PATTACH_POINT_FOLLOW, "attach_hitloc", caster:GetAbsOrigin(), true)
-    ability:ApplyDataDrivenModifier(caster, caster, "modifier_shield", {})
-    caster:AddNewModifier(caster, ability, "modifier_kisyosennfuyuzuki_shield", {})
-    caster:FindModifierByName("modifier_kisyosennfuyuzuki_shield").caster = caster
-    if caster.shield_stack == nil then
-        caster.shield_stack = 0
-    end
-    caster:FindModifierByName("modifier_shield"):SetStackCount(caster.shield_stack)
-    ability.shield_broken = false
-    
+    Timers:CreateTimer(0.1,function()
+        local caster = keys.caster
+        local ability = keys.ability
+        if caster:IsIllusion()then
+            return 
+        end
+        caster.kisyosennfuyuzuki_shield = ParticleManager:CreateParticle("particles/item_kisyosennfuyuzuki/item_kisyosennfuyuzuki.vpcf", PATTACH_ABSORIGIN_FOLLOW, caster)
+        ParticleManager:SetParticleControl(caster.kisyosennfuyuzuki_shield, 1, Vector(shield_size,0,shield_size))
+        ParticleManager:SetParticleControl(caster.kisyosennfuyuzuki_shield, 2, Vector(shield_size,0,shield_size))
+        ParticleManager:SetParticleControl(caster.kisyosennfuyuzuki_shield, 4, Vector(shield_size,0,shield_size))
+        ParticleManager:SetParticleControl(caster.kisyosennfuyuzuki_shield, 5, Vector(shield_size,0,0))
+        ParticleManager:SetParticleControlEnt(caster.kisyosennfuyuzuki_shield, 0, caster, PATTACH_POINT_FOLLOW, "attach_hitloc", caster:GetAbsOrigin(), true)
+        ability:ApplyDataDrivenModifier(caster, caster, "modifier_shield", {})
+        caster:AddNewModifier(caster, ability, "modifier_kisyosennfuyuzuki_shield", {})
+        caster:FindModifierByName("modifier_kisyosennfuyuzuki_shield").caster = caster
+        if caster.shield_stack == nil then
+            caster.shield_stack = 0
+        end
+        caster:FindModifierByName("modifier_shield"):SetStackCount(caster.shield_stack)
+        ability.shield_broken = false
+    end)
 end
 
 function OnUnequip( keys )
@@ -30,6 +34,9 @@ end
 function Take_mana_to_shield( keys )
     local caster = keys.caster
     local ability = keys.ability
+    if caster:IsIllusion() then
+        return 
+    end
     if caster:FindModifierByName("modifier_shield") == nil then
         ability:ApplyDataDrivenModifier(caster, caster, "modifier_shield", {})
         caster:AddNewModifier(caster, ability, "modifier_kisyosennfuyuzuki_shield", {})
