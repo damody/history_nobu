a1fdc1dc0cac6c3a86d533672acd652ad968633a
Victor <ts0963187279@gmail.com>
2020-04-30 17:47:30 +0800

    update cp_recover : heal value 45 => 25, +1% value per health lose 1% => +2% value per health lose 1%

--- game/dota_addons/nobu/scripts/vscripts/library/common/dummy.lua
+++ game/dota_addons/nobu/scripts/vscripts/library/common/dummy.lua
@@ -462,7 +462,8 @@ function CP_recover( keys )
   local ability = keys.ability
   local attacker = keys.attacker
   local recover = ability:GetSpecialValueFor("recover")
-  local heal = recover * (1 + attacker:GetHealth()/attacker:GetMaxHealth())
+  local recover_ampify = ability:GetSpecialValueFor("recover_ampify")
+  local heal = recover * (1 + (attacker:GetHealth()/attacker:GetMaxHealth())*recover_ampify/100)
   attacker:SetHealth(attacker:GetHealth() + heal)
 end
 
