1ec03d69db6e280a57a17467a6bec3eb9596e708
Sxty1 <circus0983@gmail.com>
2020-05-18 21:09:23 +0800

    最上NERF降治療

--- game/dota_addons/nobu/scripts/vscripts/equilibrium_constant.lua
+++ game/dota_addons/nobu/scripts/vscripts/equilibrium_constant.lua
@@ -7,7 +7,8 @@
 
 -- 
 local HP_PER_STR = 25
-local HP_REGEN_PER_STR = 0.25local MANA_PER_INT = 18
+local HP_REGEN_PER_STR = 0.25
+local MANA_PER_INT = 18
 local MANA_REGEN_PER_INT = 0.15
 local ARMOR_PER_AGI = 0.2
 local ATKSPD_PER_AGI = 1.5
