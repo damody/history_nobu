2be51a862a1e2fc66608a55bcd15c6aeb41b1764
Victor <ts0963187279@gmail.com>
2020-10-12 23:25:01 +0800

    fix bug : http request result incorrect

--- game/dota_addons/nobu/scripts/vscripts/libraries/record.lua
+++ game/dota_addons/nobu/scripts/vscripts/libraries/record.lua
@@ -5,7 +5,7 @@ if RECORD == nil then
 end
 
 function SendHTTPRequest_record(path, method, values, callback)
-    local req = CreateHTTPRequestScriptVM(method, "http://103.29.70.64:7878/record/" .. path)
+    local req = CreateHTTPRequestScriptVM(method, "https://nobu.gg/clientApi/record/" .. path)
     for key, value in pairs(values) do
         req:SetHTTPRequestGetOrPostParameter(key, value)
     end
