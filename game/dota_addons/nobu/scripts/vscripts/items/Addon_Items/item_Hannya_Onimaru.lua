833ab034d31eeada55eefa72c214fdbe31084aab
circus0983@gmail.com <aA0874412345>
2020-11-03 19:24:34 +0800

    update record

---
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_Hannya_Onimaru.lua
@@ -0,0 +1,151 @@
+
+-- modifier_Hannya_Onimaru = class({})
+
+
+
+
+-- function modifier_Hannya_Onimaru:DeclareFunctions()
+--   local funcs = {
+--   MODIFIER_EVENT_ON_UNIT_MOVED
+--   }
+--   return funcs
+-- end
+
+-- function modifier_Hannya_Onimaru:OnUnitMoved( params )
+--   if IsServer() then
+--     local caster = keys.caster
+--     print("123")
+--     print(caster.GetAbsOrigin())
+--     return caster.GetAbsOrigin()
+--   end
+--   return 0
+-- end
+
+
+function Moved ( keys )
+  local caster = keys.caster
+  local ability = keys.ability
+  local modifier = caster:FindModifierByName("modifier_Hannya_Onimaru")
+  if enemy == nil then
+    local enemy = {}
+  end
+  if caster:GetTeamNumber() == 2 then
+    enemy = FindUnitsInRadius(
+    DOTA_TEAM_GOODGUYS
+    ,caster:GetAbsOrigin()
+    ,nil
+    ,700
+    ,DOTA_UNIT_TARGET_TEAM_ENEMY
+    ,DOTA_UNIT_TARGET_HERO 
+    ,DOTA_UNIT_TARGET_FLAG_NONE
+    ,0
+  ,false)
+ -- PrintTable(enemy)
+  else
+    enemy = FindUnitsInRadius(
+    DOTA_TEAM_BADGUYS
+    ,caster:GetAbsOrigin()
+    ,nil
+    ,700
+    ,DOTA_UNIT_TARGET_TEAM_ENEMY
+    ,DOTA_UNIT_TARGET_HERO 
+    ,DOTA_UNIT_TARGET_FLAG_NONE
+    ,0
+    ,false)
+    -- PrintTable(enemy)
+  end
+
+  for i,v in pairs(enemy) do
+    local a = v:GetAbsOrigin() - caster:GetAbsOrigin() 
+    a = a:Normalized()
+    b = caster:GetForwardVector()
+    local dot = a[1] * b[1] + a[2] * b[2] + a[3] * b[3]
+    local angle = math.acos(dot / (a:Length() * b:Length()))
+    if math.deg(angle) < 90 then
+      print("find enemy") 
+      if modifier == nil then
+        ability:ApplyDataDrivenModifier(caster,caster,"modifier_Hannya_Onimaru",{})
+        modifier = caster:FindModifierByName("modifier_Hannya_Onimaru")
+        modifier:SetStackCount(1)
+        caster.Hannya_Onimaru = 1
+      end
+    else
+      print("not find")
+    end
+  end
+end
+
+function speedup (keys)
+  local caster = keys.caster
+  local ability = keys.ability
+  local modifier = caster:FindModifierByName("modifier_Hannya_Onimaru")
+  local count = modifier:GetStackCount()
+
+  if enemy == nil then
+    local enemy = {}
+  end
+  if caster:GetTeamNumber() == 2 then
+    enemy = FindUnitsInRadius(
+    DOTA_TEAM_GOODGUYS
+    ,caster:GetAbsOrigin()
+    ,nil
+    ,700
+    ,DOTA_UNIT_TARGET_TEAM_ENEMY
+    ,DOTA_UNIT_TARGET_HERO 
+    ,DOTA_UNIT_TARGET_FLAG_NONE
+    ,0
+  ,false)
+  else
+    enemy = FindUnitsInRadius(
+    DOTA_TEAM_BADGUYS
+    ,caster:GetAbsOrigin()
+    ,nil
+    ,700
+    ,DOTA_UNIT_TARGET_TEAM_ENEMY
+    ,DOTA_UNIT_TARGET_HERO 
+    ,DOTA_UNIT_TARGET_FLAG_NONE
+    ,0
+    ,false)
+  end
+
+  local leng = #enemy
+  if leng < 1 then
+    caster.Hannya_Onimaru = nil
+  end
+
+  for i,v in pairs(enemy) do
+    local a = v:GetAbsOrigin() - caster:GetAbsOrigin() 
+    a = a:Normalized()
+    b = caster:GetForwardVector()
+    local dot = a[1] * b[1] + a[2] * b[2] + a[3] * b[3]
+    local angle = math.acos(dot / (a:Length() * b:Length()))
+    if math.deg(angle) > 90 then
+      modifier:SetStackCount(0)
+      caster.Hannya_Onimaru = nil
+    else
+      caster.SPDUP = 1
+      if not caster.Hannya_Onimaru then
+      modifier:SetStackCount(1)
+      caster.Hannya_Onimaru = 1
+    end
+    end
+  end
+
+  if caster.Hannya_Onimaru then
+    if caster.Hannya_Onimaru < 4 then
+      if count < 4 then
+        count = count +1
+      end
+      modifier:SetStackCount(count)
+      caster.Hannya_Onimaru = count 
+      ability:ApplyDataDrivenModifier(caster,caster,"modifier_Hannya_Onimaru",{})
+    end
+    if caster.Hannya_Onimaru == 4 then
+      modifier:SetStackCount(count)
+      caster.Hannya_Onimaru = count 
+      ability:ApplyDataDrivenModifier(caster,caster,"modifier_Hannya_Onimaru",{})
+    end
+  else
+    modifier:SetStackCount(0)
+  end
+end
---
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_Shintogo_Kunimitsu.lua
@@ -0,0 +1,43 @@
+function TakenDamage ( keys )
+    local target = keys.caster
+    local ability = keys.ability
+    local modifier = target:FindModifierByName("modifier_Shintogo_Kunimitsu")
+    if modifier == nil then
+        ability:ApplyDataDrivenModifier(target,target,"modifier_Shintogo_Kunimitsu",{})
+        modifier = target:FindModifierByName("modifier_Shintogo_Kunimitsu")
+        modifier:SetStackCount(1)
+        target.Shintogo_Kunimitsu = 1
+        target.magical_resistance = target.magical_resistance + 3
+    else
+        local count = modifier:GetStackCount() + 1
+        if count >= 5 then
+            count = 5
+        end
+        if target.Shintogo_Kunimitsu < 5 then
+            target.magical_resistance = target.magical_resistance + 3
+            modifier:SetStackCount(count)
+            ability:ApplyDataDrivenModifier(target,target,"modifier_Shintogo_Kunimitsu",nil)
+            target.Shintogo_Kunimitsu = count
+        elseif target.Shintogo_Kunimitsu == 5 then
+            modifier:SetStackCount(count)
+            ability:ApplyDataDrivenModifier(target,target,"modifier_Shintogo_Kunimitsu",nil)
+            target.Shintogo_Kunimitsu = count
+        end
+        
+    end
+end
+
+
+function OnDestroy( keys )
+    local target = keys.caster
+    local ability = keys.ability
+    if target.Shintogo_Kunimitsu > 0 then
+        target.Shintogo_Kunimitsu = target.Shintogo_Kunimitsu - 1
+        target.magical_resistance = target.magical_resistance - 3
+        if target.Shintogo_Kunimitsu >= 1 then
+            ability:ApplyDataDrivenModifier(target,target,"modifier_Shintogo_Kunimitsu",nil)
+            local modifier = target:FindModifierByName("modifier_Shintogo_Kunimitsu")
+            modifier:SetStackCount(target.Shintogo_Kunimitsu)
+        end
+    end
+end
