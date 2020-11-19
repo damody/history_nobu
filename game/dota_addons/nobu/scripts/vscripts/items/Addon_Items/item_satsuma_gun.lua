59f2c9f1c4707cdd059e40459fb1beb0c608df42
circus0983@gmail.com <aA0874412345>
2020-11-19 17:36:47 +0800

    fix satsuma sound

--- game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_satsuma_gun.lua
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_satsuma_gun.lua
@@ -45,7 +45,7 @@ function OnAttackLanded( keys )
                 iVisionTeamNumber	= caster:GetTeamNumber(),
             }
             projectile_table.vVelocity = dir*2000
-            caster:EmitSound( "ability.Assassinate" )
+            caster:EmitSound( "Hero_Sniper.Attack" )
             if caster.satsumaShot == true then
                 caster.satsuma = 0
                 ProjectileManager:CreateLinearProjectile(projectile_table)
@@ -65,42 +65,10 @@ function OnProjectileHit ( keys )
     local caster = keys.caster
     local target = keys.target
     local ability = keys.ability
+    local damage = caster:GetAverageTrueAttackDamage(target)
     if not target:IsMagicImmune() then
-        caster:PerformAttack(target, true, true, true, true, false, false, false)
+        AMHC:Damage(caster,target,damage,AMHC:DamageType( "DAMAGE_TYPE_PHYSICAL" ) )
+        caster:PerformAttack(target,true,true,true,false,false,true,true)
     end
-end
-
-
-function Shock( keys )
-    local caster = keys.caster
-    local target = keys.target
-    local ability = keys.ability
-    local casterabs = caster:GetAbsOrigin()
-    local targetabs = target:GetAbsOrigin()
-    local angle = VectorToAngles(targetabs-casterabs).y
-	local dx = math.cos(angle*(3.14/180))
-	local dy = math.sin(angle*(3.14/180))
-    local dir = Vector(dx,dy,0)
-    projectile_table = {
-		Ability				= ability,
-		EffectName			= "particles/a17w/a17w.vpcf",
-		vSpawnOrigin		= casterabs+Vector(0,0,100),
-		fDistance			= 1200,
-		fStartRadius		= 100,
-		fEndRadius			= 100,
-		Source				= caster,
-		bHasFrontalCone		= true,
-		bReplaceExisting	= false,
-		iUnitTargetTeam		= ability:GetAbilityTargetTeam(),
-		iUnitTargetFlags	= ability:GetAbilityTargetFlags(),
-		iUnitTargetType		= ability:GetAbilityTargetType(),
-		fExpireTime			= GameRules:GetGameTime() + 10,
-		bDeleteOnHit		= false,
-		vVelocity			= 2000,
-		bProvidesVision		= false,
-		iVisionRadius		= 0,
-		iVisionTeamNumber	= caster:GetTeamNumber(),
-    }
-	ProjectileManager:CreateLinearProjectile(projectile_table)
 end
 
