81d6be58f96dc7a364e84092134ce34241d34dca
circus0983@gmail.com <aA0874412345>
2020-12-24 14:58:48 +0800

    adjust soldier damage to soldier

--- game/dota_addons/nobu/scripts/vscripts/library/common/dummy.lua
+++ game/dota_addons/nobu/scripts/vscripts/library/common/dummy.lua
@@ -480,9 +480,8 @@ function modifier_unit_armor:DeclareFunctions()
 end
 
 function modifier_unit_armor:GetModifierIncomingDamage_Percentage( keys )
-  print(keys.attacker:GetUnitName())
   if string.match(keys.attacker:GetName(), "npc_dota_creep_lane") then 
-    return 500
+    return 50
   elseif keys.attacker:IsBuilding() then
     if self.creep then
       return -100
