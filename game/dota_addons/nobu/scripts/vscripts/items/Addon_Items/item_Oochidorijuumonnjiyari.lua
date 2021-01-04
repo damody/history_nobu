e044060b21dbda143325b5aa923e098372309042
circus0983@gmail.com <aA0874412345>
2021-01-04 17:25:56 +0800

    新增大千鳥十文字槍

---
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_Oochidorijuumonnjiyari.lua
@@ -0,0 +1,29 @@
+function Shock ( keys )
+    local caster = keys.caster
+    local target = keys.target
+    local ability = keys.ability
+
+    local group = FindUnitsInRadius(caster:GetTeamNumber(),
+    target:GetAbsOrigin(),
+    nil,
+    500,
+    DOTA_UNIT_TARGET_TEAM_ENEMY,
+    DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
+    DOTA_UNIT_TARGET_FLAG_FOW_VISIBLE,
+    FIND_ANY_ORDER,
+    false)
+    for i,unit in ipairs(group) do
+        if unit:IsHero() then
+            local info = {
+                Target = unit,
+                Source = caster,
+                EffectName = "particles/a34w/a34w.vpcf",
+                Ability = ability,
+                iMoveSpeed = 2000,
+                vSpawnOrigin = caster:GetAbsOrigin()
+            }
+            ProjectileManager:CreateTrackingProjectile ( info )
+        end
+    end
+
+end
