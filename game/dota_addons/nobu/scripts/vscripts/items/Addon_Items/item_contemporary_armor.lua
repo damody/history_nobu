471102847f7eef27c02c88f8be5e67d9f848e48e
Victor <ts0963187279@gmail.com>
2021-03-02 16:43:50 +0800

    update item_contemporary_armor: add new feature when spell start: nearby hero and build can block 200 damage for 12 second, can not take buff for 40 second

--- game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_contemporary_armor.lua
+++ game/dota_addons/nobu/scripts/vscripts/items/Addon_Items/item_contemporary_armor.lua
@@ -27,3 +27,45 @@ function OnUnEquip ( keys )
     end
     
 end
+
+function Shock( keys )
+    local caster = keys.caster
+    local ability = keys.ability
+    local block_duration = ability:GetSpecialValueFor("block_duration")
+    local block_cd = ability:GetSpecialValueFor("block_cd")
+    local block_radius = ability:GetSpecialValueFor("block_radius")
+    local groups = FindUnitsInRadius(caster:GetTeamNumber(),
+        caster:GetAbsOrigin(),
+        nil,
+        block_radius,
+        DOTA_UNIT_TARGET_TEAM_FRIENDLY,
+        DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BUILDING,
+        DOTA_UNIT_TARGET_FLAG_NONE,
+        0,
+        false)
+    for _,target in pairs(groups) do
+        if not target:HasModifier("modifier_contemporary_armor_block_cd") then
+            ability:ApplyDataDrivenModifier(caster, target, "modifier_contemporary_armor_block", {duration = block_duration})
+            ability:ApplyDataDrivenModifier(caster, target, "modifier_contemporary_armor_block_cd", {duration = block_cd})
+            target.contemporary_armor_shield = ParticleManager:CreateParticle("particles/econ/items/pangolier/pangolier_ti8_immortal/pangolier_ti8_immortal_shield_buff.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
+            Timers:CreateTimer(0, function() 
+                if IsValidEntity(target) then
+                    ParticleManager:SetParticleControl(target.contemporary_armor_shield,1,target:GetAbsOrigin()+Vector(0, 0, 30))
+                else
+                    isend = true
+                end
+                if not isend then
+                    return 0.05
+                else
+                    ParticleManager:DestroyParticle(target.contemporary_armor_shield, false)
+                    return nil
+                end
+            end)
+        end
+    end
+end
+
+function OnDestroy( keys )
+    local target = keys.target
+    ParticleManager:DestroyParticle(target.contemporary_armor_shield, false)
+end
