c86919559d0b0e7b1ed5fc2f73754cfb678b40c1
Victor <ts0963187279@gmail.com>
2021-03-08 14:23:10 +0800

    enable radar

--- game/dota_addons/nobu/scripts/vscripts/library/events/eventfororder.lua
+++ game/dota_addons/nobu/scripts/vscripts/library/events/eventfororder.lua
@@ -859,12 +859,12 @@ function Nobu:eventfororder( filterTable )
 
 	elseif ordertype == DOTA_UNIT_ORDER_TAUNT	 then --22
 	elseif ordertype == DOTA_UNIT_ORDER_BUYBACK	 then --23
-	elseif ordertype == DOTA_UNIT_ORDER_GLYPH	 then --24
+	elseif ordertype == DOTA_UNIT_ORDER_GLYPH	 then --24  防御浮雕
 		return false
 	elseif ordertype == DOTA_UNIT_ORDER_EJECT_ITEM_FROM_STASH	 then --25
 	elseif ordertype == DOTA_UNIT_ORDER_CAST_RUNE	 then --26
-	elseif ordertype == 31	 then --24
-		return false
+	elseif ordertype == 31	 then --24 雷達
+		return true
 	elseif ordertype ==	33	 then --案s一開始
 		-- DeepPrintTable(filterTable)
 		-- [   VScript              ]: {
